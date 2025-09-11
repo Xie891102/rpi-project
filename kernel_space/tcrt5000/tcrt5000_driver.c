2be458768ed478094fd3ae86a4b7600f983df2ee
xie_rpi <xie5176@gmail.com>
2025-09-11 12:24:10 +0800

    complete開機自動掛載、關機自動卸載(modules、device node、permission)

--- kernel_space/tcrt5000/tcrt5000_driver.c
+++ kernel_space/tcrt5000/tcrt5000_driver.c
@@ -3,10 +3,16 @@
 #include <linux/module.h>
 #include <linux/fs.h>
 #include <linux/uaccess.h>
+#include <linux/device.h>
+#include <linux/cdev.h>
 #include "pin_mapping.h"
 #include "tcrt5000_hal.h"
+	
+// ---------- 全域變數 ------------
+static dev_t dev;		// 儲存分配到的 major/minor 
+static struct cdev c_dev;	// 字元裝置的結構體
+static struct class *cl;	// class 用來 /dev 創建節點
 
-static int major;		// 定義全域major number
 
 
 // ---------- 檔案處理 -------------
@@ -61,22 +67,64 @@ static struct file_operations fops = {
 };
 
 
+// 定義 devnode callback，設定 /dev/tcrt5000 權限 改成0666
+static char *tcrt5000_devnode(const struct device *dev, umode_t *mode){
+	if(mode) *mode = 0666;	// 所有人都可以寫入
+	return NULL;		// 預設裝置名稱
+}
+
+
 // ------------ 初始化 -------------
 
 // 註冊device，取得major number
 static int __init tcrt5000_driver_init(void){
 	
-	// 0.註冊字元裝置 0 => kernel自動分配
-	major = register_chrdev(0, "tcrt5000", &fops);
+	int ret;
+	printk(KERN_INFO "TCRT5000: Initializing driver...\n");
 
-	// 如果沒有數字 代表失敗
-	if(major < 0){
-		printk(KERN_ALERT "Register char device failed\n");
-		return major;
+	// 1.分配 major + minor，讓Kernel自動分配裝置號碼
+	ret = alloc_chrdev_region(&dev, 0, 1, "tcrt5000");
+	if(ret < 0){
+		printk(KERN_ALERT "TCRT5000: Failed to allocate char device region\n");
+		return ret;
 	}
-	
+	printk(KERN_INFO "TCRT5000 major number allocated= %d\n", MAJOR(dev));
+	printk(KERN_INFO "TCRT5000 minor number= %d\n"	, MINOR(dev));
+
+
+	// 2.初始化 cdev 結構體，並連結到 file operations
+	cdev_init(&c_dev, &fops);
+	c_dev.owner = THIS_MODULE;			// 防止卸載時 還有再操作
+
+
+	// 3.正式註冊裝置，將 cdev 加入 kernel
+	ret = cdev_add(&c_dev, dev, 1);			// 裝置的數量 1
+	if(ret < 0){
+		unregister_chrdev_region(dev, 1);	// 取消註冊 釋放資源
+		printk(KERN_ALERT "TCRT5000: Failed to add cdev\n");
+		return ret;
+	}	
+
+
+	// 4.創建 class，用於 /dev 下自動生成節點
+	cl = class_create("tcrt5000_class");
+	if(IS_ERR(cl)){					// 如果有錯誤
+		cdev_del(&c_dev);			// 先刪除 cdev
+		unregister_chrdev_region(dev, 1);	// 釋放 major/minor
+		printk(KERN_ALERT "TCRT5000: Failed to create class\n");
+		return PTR_ERR(cl);
+	}
+
+	// 指定 devnode callback
+	cl->devnode = tcrt5000_devnode;
+
+
+	// 5.正式創建 /dev/tcrt5000 節點，裝置的入口 讓應用層可以直接用
+	device_create(cl, NULL, dev, NULL, "tcrt5000");
+
+
 	// 成功註冊
-	printk(KERN_INFO "TCRT5000 driver loaded, major number: %d\n", major);
+	printk(KERN_INFO "TCRT5000 driver loaded successfully!\n");
 	return 0;
 }
 
@@ -84,12 +132,20 @@ static int __init tcrt5000_driver_init(void){
 // 卸載 解除註冊 device
 static void __exit tcrt5000_driver_exit(void){
 	
-	// 解除註冊
-	unregister_chrdev(major, "tcrt5000");
-	printk(KERN_INFO "TCRT5000 driver unloaded\n");
-}
+	// 1.刪除 device node
+	device_destroy(cl, dev);
 
+	// 2.刪除 class
+	class_destroy(cl);
 	
+	// 3.刪除 cdev 結構體
+	cdev_del(&c_dev);
+
+	// 4.釋放資源 major/minor
+	unregister_chrdev_region(dev, 1);
+
+	printk(KERN_INFO "TCRT5000 driver unloaded successfully\n");
+}
 
 
 // 設定 module 進入點、退出點
@@ -100,7 +156,7 @@ module_exit(tcrt5000_driver_exit);	// rmmod 時呼叫
 // 設定 module 資訊
 MODULE_LICENSE("GPL");			// 授權GPL，避免被視為私有模組
 MODULE_AUTHOR("XIE");			// 作者
-MODULE_DESCRIPTION("TCRT5000 Device Driver"); 	// 簡短說明
+MODULE_DESCRIPTION("TCRT5000 Device Driver connected auto Device Node"); 	// 簡短說明
 
 
 
